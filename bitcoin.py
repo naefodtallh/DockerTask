82d2cdefc8bb995462516c8fe60a43f1fc1336ed
naef odtallh <naef2595@gmail.com>
2021-10-15 22:19:18 +0300

    add the requirements file

--- bitcoin.py
+++ bitcoin.py
@@ -1,12 +1,16 @@
 # Get the current price of Bitcoin in USD using the coindesk.com API with Python and the Requests HTTP library
+from flask import Flask
+import requests 
+app = Flask(__name__)
 
-import requests # http://docs.python-requests.org/en/master/
-
+@app.route('/')
+@app.route('/bitcoin')
+def bitcoin():
 btc = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+print(btc.json()['bpi']['USD']['rate'])        #print(btc.status_code)
+                                               #print(btc.headers['content-type'])
+                                               #print(btc.encoding)
+                                               #print(btc.text)
 
-#print(btc.status_code)
-#print(btc.headers['content-type'])
-#print(btc.encoding)
-#print(btc.text)
-
-print(btc.json()['bpi']['USD']['rate'])
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
